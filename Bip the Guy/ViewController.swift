8881cad7d0708c7667a28deb6e65eb77ef877391
Matthew Spana <spanam@bc.edu>
2020-02-06 01:18:48 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by Matthew Spana on 2/6/20.
+//  Copyright © 2020 Matthew Spana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
